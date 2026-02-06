3d4da7f900e06eb6dd2e5d0d1524951d15c59c30
thpks <thpks@LAPTOP-2Q8IF4VK>
2026-02-06 12:24:13 +0800

    Add project files.

---
+++ INFT2051_W1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace INFT2051_W1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        int countMore = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnMoreCounterClicked(object? sender, EventArgs e)
+        {
+            countMore += 2;
+            CounterMoreBtn.Text = $"Clicked {countMore} times";
+            SemanticScreenReader.Announce((CounterMoreBtn.Text));
+        }
+    }
+}
---
+++ INFT2051_W1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace INFT2051_W1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
